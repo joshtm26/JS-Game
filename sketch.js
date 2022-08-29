a78738b4dd903765ca70c57aacdf577aa168a5eb
Glitch (p5js-blank-sketch-template) <none>
2022-08-29 16:23:08 +0000

    🏖🚵 Checkpoint ./sketch.js:8580995/544 ./.eslintrc.json:8580995/95

--- sketch.js
+++ sketch.js
@@ -1,10 +1,8 @@
 function setup() {
   createCanvas(400, 400);
-  square(mouseX, mouseY);
-  circle(pmouseX, pmouseY);
-  triangle(0, 0, 400, 0, 0, 400);
-  mouseX = 5;
-  mouseY = 10;
 }
 
-function draw() {}
+function draw() {
+  square(mouseX, mouseY, width / 10);
+  circle(pmouseX, pmouseY, width / 10);
+}
