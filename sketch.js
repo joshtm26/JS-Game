59ccf8720d65b475bd7ea629f6e3e34efd3f689e
Glitch (p5js-blank-sketch-template) <none>
2022-10-25 19:24:45 +0000

    ⛱💨 Checkpoint ./sketch.js:48522715/617

--- sketch.js
+++ sketch.js
@@ -1,17 +1,20 @@
 //TO DO
 
 //back and forth dashing/stepping
+//reset positions and display text for winner when sword hits player
 //2 attacks: a slower poke or longer swipe and quick forward down swing
 //animations for movement, attacking, and maybe idle
 //ending lag for the actions somehow
 //make the weapons look better (two different weapons?)
 //add sounds to the actions
 //make a background maybe
-//maybe make it not stick people if i have lots of extra time
+//maybe make it not stick people
 
 let bg;
 let ldx = 0;
+let ldy = 0;
 let rdx = 0;
+let rdy = 0;
 
 // function preload() {
 //   bg = loadImage("assets/stage.jpg");
@@ -39,7 +42,7 @@ function draw() {
   if (keyIsDown(65) && ldx >= -60) {
     ldx = ldx - 5;
   }
-  //D
+  //D 68
   if (keyIsDown(68) && ldx >= -70) {
     ldx = ldx + 5;
   }
@@ -57,26 +60,26 @@ function draw() {
 function lDude() {
   fill(0);
   stroke(0);
-  circle(ldx + 105, 160, 50);
-  line(ldx + 100, 150, ldx + 90, 260);
+  circle(ldx + 105, ldy + 160, 50);
+  line(ldx + 100, ldy + 150, ldx + 90, ldy + 260);
   //legs
-  line(ldx + 90, 260, ldx + 80, 305);
-  line(ldx + 80, 305, ldx + 60, 350);
-  line(ldx + 90, 260, ldx + 115, 305);
-  line(ldx + 115, 305, ldx + 110, 350);
+  line(ldx + 90, ldy + 260, ldx + 80, ldy + 305);
+  line(ldx + 80, ldy + 305, ldx + 60, ldy + 350);
+  line(ldx + 90, ldy + 260, ldx + 115, ldy + 305);
+  line(ldx + 115, ldy + 305, ldx + 110, ldy + 350);
   //arms
-  line(ldx + 96, 210, ldx + 112, 240);
-  line(ldx + 112, 240, ldx + 140, 250);
-  line(ldx + 96, 202, ldx + 120, 230);
-  line(ldx + 120, 230, ldx + 145, 235);
+  line(ldx + 96, ldy + 210, ldx + 112, ldy + 240);
+  line(ldx + 112, ldy + 240, ldx + 140, ldy + 250);
+  line(ldx + 96, ldy + 202, ldx + 120, ldy + 230);
+  line(ldx + 120, ldy + 230, ldx + 145, ldy + 235);
 }
 
 function lSword() {
   fill(0);
   noStroke();
-  quad(lsx + 135, 260, lsx + 143, 262, lsx + 153, 225, lsx + 145, 223);
+  quad(ldx + 135, ldy + 260, ldx + 143, ldy + 262, ldx + 153, ldy + 225, ldx + 145, ldy + 223);
   fill(240);
-  quad(lsx + 153, 225, lsx + 145, 223, lsx + 177, 103, lsx + 182, 115);
+  quad(ldx + 153, ldy + 225, ldx + 145, ldy + 223, ldx + 177, ldy + 103, ldx + 182, ldy + 115);
 }
 
 function rDude() {
