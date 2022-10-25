bd0114df1b5b7b53c986804208dfe32dc6d544c2
Glitch (p5js-blank-sketch-template) <none>
2022-10-25 18:49:43 +0000

    📅🌠 Checkpoint ./sketch.js:48522715/5054

--- sketch.js
+++ sketch.js
@@ -1,25 +1,26 @@
 //TO DO
 
 //back and forth dashing/stepping
-//2 players one keyboard
 //2 attacks: a slower poke/longer swipe and quick forward down swing
 //animations for movement, attacking, and maybe idle
 //ending lag for the actions somehow
-//make the weapons look better
+//make the weapons look better (two different weapons?)
+//add sounds to the actions
 //make a background maybe
 //maybe make it not stick people if i have lots of extra time
 
-let img;
+let bg;
 let ldx = 0;
 let rdx = 0;
+
 // function preload() {
-//   img = loadImage("assets/stage.jpg");
+//   bg = loadImage("assets/stage.jpg");
 // }
 
 function setup() {
   createCanvas(800, 400);
   angleMode(DEGREES);
-  //image(img, 0, 0, width, height, 0, 0, img.width, img.height, COVER);
+  //image(bg, 0, 0, width, height, 0, 0, bg.width, bg.height, COVER);
 }
 
 function draw() {
@@ -41,11 +42,11 @@ function draw() {
     ldx = ldx + 5;
   }
   //Left arrow
-  if (keyIsDown(LEFT_ARROW) && rdx <= ) {
+  if (keyIsDown(LEFT_ARROW) && rdx <= 80) {
     rdx = rdx - 5;
   }
   //Right arrow
-  if (keyIsDown(RIGHT_ARROW) && rdx > 0) {
+  if (keyIsDown(RIGHT_ARROW) && rdx <= 80) {
     rdx = rdx + 5;
   }
   //W/S and Up/Down arrows are going to be the two attacks
@@ -54,23 +55,19 @@ function draw() {
 function lDude() {
   fill(0);
   stroke(0);
-  //head
   circle(ldx + 105, 160, 50);
-  //torso
   line(ldx + 100, 150, ldx + 90, 260);
-  //left leg
+  //legs
   line(ldx + 90, 260, ldx + 80, 305);
   line(ldx + 80, 305, ldx + 60, 350);
-  //right leg
   line(ldx + 90, 260, ldx + 115, 305);
   line(ldx + 115, 305, ldx + 110, 350);
-  //left arm
+  //arms
   line(ldx + 96, 210, ldx + 112, 240);
   line(ldx + 112, 240, ldx + 140, 250);
-  //right arm
   line(ldx + 96, 202, ldx + 120, 230);
   line(ldx + 120, 230, ldx + 145, 235);
-  
+
   //sword
   fill(0);
   noStroke();
@@ -82,23 +79,19 @@ function lDude() {
 function rDude() {
   fill(0);
   stroke(0);
-  //head
   circle(rdx + 695, 160, 50);
-  //torso
   line(rdx + 700, 150, rdx + 710, 260);
-  //left leg
+  //legs
   line(rdx + 710, 260, rdx + 720, 305);
   line(rdx + 720, 305, rdx + 740, 350);
-  //right leg
   line(rdx + 710, 260, rdx + 685, 305);
   line(rdx + 685, 305, rdx + 690, 350);
-  //left arm
+  //arms
   line(rdx + 704, 210, rdx + 688, 240);
   line(rdx + 688, 240, rdx + 660, 250);
-  //right arm
   line(rdx + 704, 202, rdx + 680, 230);
   line(rdx + 680, 230, rdx + 655, 235);
-  
+
   //sword
   fill(0);
   noStroke();
