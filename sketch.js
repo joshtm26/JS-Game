a49288bb08ebcc0f2c012e0381e8966424975601
Glitch (p5js-blank-sketch-template) <none>
2022-11-02 19:05:21 +0000

    🚍📹 Checkpoint ./sketch.js:48522715/10605

--- sketch.js
+++ sketch.js
@@ -1,11 +1,8 @@
 //TO DO
 
-//Create objects for my each of my guys
-//properties: pos
-//methods: walk, dash, attack1, attack2
-
 //back and forth dashing/stepping
-//2 attacks: a slower poke or longer swipe and quick forward down swing
+//attack for both players
+//
 //ending lag for the actions
 //reset positions and display text for winner when sword hits player
 //animations for movement, attacking, and idle
@@ -20,14 +17,13 @@
 //make my own background
 //zoom in and make camera follow them
 
-
 let bgm;
 let bg;
 
-let ldx = 0;
-let ldy = 0;
-let rdx = 0;
-let rdy = 0;
+let lpx = 0;
+let lpy = 0;
+let rpx = 0;
+let rpy = 0;
 
 let count = 0;
 let dPress = 0;
@@ -70,26 +66,26 @@ function draw() {
   pop();
 
   //A
-  if (keyIsDown(65) && ldx >= -100) {
-    ldx = ldx - 3;
+  if (keyIsDown(65) && lpx >= -100) {
+    lpx -= 3;
   }
   //D
-  if (keyIsDown(68) && ldx <= rdx + 470) {
-    ldx = ldx + 3;
+  if (keyIsDown(68) && lpx <= rpx + 470) {
+    lpx += 3;
   }
   //Left arrow
-  if (keyIsDown(LEFT_ARROW) && rdx >= ldx - 470) {
-    rdx = rdx - 3;
+  if (keyIsDown(LEFT_ARROW) && rpx >= lpx - 470) {
+    rpx = rpx - 3;
   }
   //Right arrow
-  if (keyIsDown(RIGHT_ARROW) && rdx <= 100) {
-    rdx = rdx + 3;
+  if (keyIsDown(RIGHT_ARROW) && rpx <= 100) {
+    rpx = rpx + 3;
   }
 
-  //W/S and Up/Down arrows are going to be the two attacks
+  //W and Up arrow are going to be the two attacks
   wKeyPressed();
   upKeyPressed();
-  
+
   print(dPress);
 }
 
@@ -115,7 +111,7 @@ function keyReleased() {
       dPress = 0;
     }
     if (dPress == 1) {
-      ldx += 60;
+      lpx += 60;
     }
   }
   //a dash
@@ -126,7 +122,7 @@ function keyReleased() {
       aPress = 0;
     }
     if (aPress == 1) {
-      ldx -= 60;
+      lpx -= 60;
     }
   }
   //left dash
@@ -137,7 +133,7 @@ function keyReleased() {
       lPress = 0;
     }
     if (lPress == 1) {
-      rdx -= 60;
+      rpx -= 60;
     }
   }
   //right dash
@@ -148,7 +144,7 @@ function keyReleased() {
       rPress = 0;
     }
     if (rPress == 1) {
-      rdx += 60;
+      rpx += 60;
     }
   }
 }
@@ -157,48 +153,48 @@ function lDude() {
   fill(0);
   stroke(0);
   strokeWeight(10);
-  circle(ldx + 105, ldy + 160, 50);
-  line(ldx + 100, ldy + 150, ldx + 90, ldy + 260);
+  circle(lpx + 105, lpy + 160, 50);
+  line(lpx + 100, lpy + 150, lpx + 90, lpy + 260);
   //legs
-  line(ldx + 90, ldy + 260, ldx + 80, ldy + 305);
-  line(ldx + 80, ldy + 305, ldx + 60, ldy + 350);
-  line(ldx + 90, ldy + 260, ldx + 115, ldy + 305);
-  line(ldx + 115, ldy + 305, ldx + 110, ldy + 350);
+  line(lpx + 90, lpy + 260, lpx + 80, lpy + 305);
+  line(lpx + 80, lpy + 305, lpx + 60, lpy + 350);
+  line(lpx + 90, lpy + 260, lpx + 115, lpy + 305);
+  line(lpx + 115, lpy + 305, lpx + 110, lpy + 350);
 }
 
 function lDudeArms() {
   fill(0);
   stroke(0);
   strokeWeight(10);
-  line(ldx + 96, ldy + 210, ldx + 112, ldy + 240);
-  line(ldx + 112, ldy + 240, ldx + 140, ldy + 250);
-  line(ldx + 96, ldy + 202, ldx + 120, ldy + 230);
-  line(ldx + 120, ldy + 230, ldx + 145, ldy + 235);
+  line(lpx + 96, lpy + 210, lpx + 112, lpy + 240);
+  line(lpx + 112, lpy + 240, lpx + 140, lpy + 250);
+  line(lpx + 96, lpy + 202, lpx + 120, lpy + 230);
+  line(lpx + 120, lpy + 230, lpx + 145, lpy + 235);
 }
 
 function lSword() {
   fill(0);
   noStroke();
   quad(
-    ldx + 135,
-    ldy + 260,
-    ldx + 143,
-    ldy + 262,
-    ldx + 153,
-    ldy + 225,
-    ldx + 145,
-    ldy + 223
+    lpx + 135,
+    lpy + 260,
+    lpx + 143,
+    lpy + 262,
+    lpx + 153,
+    lpy + 225,
+    lpx + 145,
+    lpy + 223
   );
   fill(240);
   quad(
-    ldx + 153,
-    ldy + 225,
-    ldx + 145,
-    ldy + 223,
-    ldx + 177,
-    ldy + 103,
-    ldx + 182,
-    ldy + 115
+    lpx + 153,
+    lpy + 225,
+    lpx + 145,
+    lpy + 223,
+    lpx + 177,
+    lpy + 103,
+    lpx + 182,
+    lpy + 115
   );
 }
 
@@ -206,47 +202,47 @@ function rDude() {
   fill(0);
   stroke(0);
   strokeWeight(10);
-  circle(rdx + 695, 160, 50);
-  line(rdx + 700, 150, rdx + 710, 260);
+  circle(rpx + 695, 160, 50);
+  line(rpx + 700, 150, rpx + 710, 260);
   //legs
-  line(rdx + 710, 260 + rdy, rdx + 720, 305 + rdy);
-  line(rdx + 720, 305 + rdy, rdx + 740, 350 + rdy);
-  line(rdx + 710, 260 + rdy, rdx + 685, 305 + rdy);
-  line(rdx + 685, 305 + rdy, rdx + 690, 350 + rdy);
+  line(rpx + 710, 260 + rpy, rpx + 720, 305 + rpy);
+  line(rpx + 720, 305 + rpy, rpx + 740, 350 + rpy);
+  line(rpx + 710, 260 + rpy, rpx + 685, 305 + rpy);
+  line(rpx + 685, 305 + rpy, rpx + 690, 350 + rpy);
 }
 
 function rDudeArms() {
   fill(0);
   stroke(0);
   strokeWeight(10);
-  line(rdx + 704, 210, rdx + 688, 240);
-  line(rdx + 688, 240, rdx + 660, 250);
-  line(rdx + 704, 202, rdx + 680, 230);
-  line(rdx + 680, 230, rdx + 655, 235);
+  line(rpx + 704, 210, rpx + 688, 240);
+  line(rpx + 688, 240, rpx + 660, 250);
+  line(rpx + 704, 202, rpx + 680, 230);
+  line(rpx + 680, 230, rpx + 655, 235);
 }
 
 function rSword() {
   fill(0);
   noStroke();
   quad(
-    rdx + 665,
-    260 + rdy,
-    rdx + 657,
-    262 + rdy,
-    rdx + 647,
-    225 + rdy,
-    rdx + 655,
-    223 + rdy
+    rpx + 665,
+    260 + rpy,
+    rpx + 657,
+    262 + rpy,
+    rpx + 647,
+    225 + rpy,
+    rpx + 655,
+    223 + rpy
   );
   fill(240);
   quad(
-    rdx + 647,
-    225 + rdy,
-    rdx + 655,
-    223 + rdy,
-    rdx + 623,
-    103 + rdy,
-    rdx + 618,
-    115 + rdy
+    rpx + 647,
+    225 + rpy,
+    rpx + 655,
+    223 + rpy,
+    rpx + 623,
+    103 + rpy,
+    rpx + 618,
+    115 + rpy
   );
 }
