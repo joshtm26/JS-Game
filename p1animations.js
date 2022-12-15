784ba1ba9145c68878af1754615bde50b5788c3f
Glitch (playful-prototype-josh-mullen) <none>
2022-12-15 23:46:54 +0000

    🏜🚀 Checkpoint ./p1animations.js:48522715/147

--- p1animations.js
+++ p1animations.js
@@ -12,10 +12,13 @@ class P1Animations {
         image(unsheathed, p2.x - 200, -77);
         //stop rain somehow
       }
-      if (this.specialCount >= 100) {
+      if (this.specialCount >= 100 && this.specialCount < 110) {
         image(sheathed, p2.x - 200, -77)
         p1.win = true;
         //play rain
+      }
+      if (this.specialCount >= 110) {
+        animation(p1IdleAni, p2.x + 237, 322)
       }
     }
   }
